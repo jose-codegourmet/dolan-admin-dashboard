6b1cfa0e9d1e6674ab24db3ae05fff30e1c87949
jab <jose@codegourmet.io>
2024-03-04 17:51:57 +0800

    chore: added menu constants

---
+++ src/constants/menu.tsx
@@ -0,0 +1,59 @@
+import { AppRoutes } from "./routes";
+import {
+  TbBuildingWarehouse,
+  TbGardenCart,
+  TbHome,
+  TbReceipt,
+  TbTableAlias,
+  TbUserCog,
+  TbUserDollar,
+  TbUsers,
+} from "react-icons/tb";
+
+export const MAIN_MENU = [
+  {
+    title: "Dashboard",
+    route: AppRoutes.Dashboard,
+    icon: <TbHome />,
+  },
+  {
+    title: "Orders",
+    route: AppRoutes.Orders,
+    icon: <TbReceipt />,
+  },
+  {
+    title: "Products",
+    route: AppRoutes.Products,
+    icon: <TbGardenCart />,
+    submenus: [
+      {
+        title: "Collection",
+        route: AppRoutes.Collection,
+        icon: <TbTableAlias />,
+      },
+      {
+        title: "Storage",
+        route: AppRoutes.Storage,
+        icon: <TbBuildingWarehouse />,
+      },
+    ],
+  },
+  {
+    title: "Customers",
+    route: AppRoutes.Customers,
+    icon: <TbUserDollar />,
+  },
+];
+
+export const SETTINGS_MENU = [
+  {
+    title: "Staff",
+    route: AppRoutes.Staff,
+    icon: <TbUsers />,
+  },
+  {
+    title: "Staff Types",
+    route: AppRoutes.StaffTypes,
+    icon: <TbUserCog />,
+  },
+];
---
+++ src/constants/routes.ts
@@ -0,0 +1,33 @@
+export enum AppRoutes {
+  Collection = "/collection",
+  NewCollection = "/collection/new",
+
+  Products = "/products",
+  NewProduct = "/products/new",
+
+  Storage = "/storage",
+  NewStorage = "/storage/new",
+
+  Customers = "/customers",
+  NewCustomer = "/customers/new",
+
+  Orders = "/orders",
+  NewOrder = "/orders/new",
+
+  DraftOrders = "/draft-orders",
+  NewDraftOrder = "/draft-orders/new",
+
+  Notifications = "/notifications",
+  Reports = "/reports",
+
+  Settings = "/settings",
+  Staff = "/settings/staff",
+  NewStaff = "/settings/staff/new",
+  StaffTypes = "/settings/staff-types",
+  NewStaffType = "/settings/staff-types/new",
+  MyAccount = "/settings/my-account",
+
+  Dashboard = "/dashboard",
+  Login = "/login",
+  Home = "/",
+}
